1be0be3de6a4ea47d18e6a9d0ab8bd30eeaa7170
eXe <leexgo1987Qgmail.com>
2023-12-04 23:47:11 -0300

    Configurando preloading en el app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideHttpClient()]
+  providers: [
+    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)), 
+    provideHttpClient()]
 };
